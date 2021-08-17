30fec1af519ac8f3b336d99bef90a0a6e6028b78
HarryPotter12352 <harrypottr12352.github@outlook.com>
2021-08-17 11:03:16 +0530

    Added urls, it was lacking before

--- signup_login/signup_login/urls.py
+++ signup_login/signup_login/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.index, name = "index"),
+    path("signup", views.signup, name = "signup"),
+    path("signuped", views.signedup, name = "signedup"),
+    path("login", views.login, name = "login"),
+    path("loggedin", views.loggedin, name = "loggedin")
 ]
